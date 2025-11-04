10a900683e2ad675fda0c3264d2ef9ea3c75052e
daren-scerri <daren.scerri@mcast.edu.mt>
2025-11-04 11:27:20 +0100

    RequireComponent Example

---
+++ Inheritance_Composition_Lecture/Assets/Scripts/Composition Scripts/BulletControl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent (typeof(FireUp))]
+public class BulletControl : MonoBehaviour
+{
+   
+
+    Rigidbody2D rb;
+    FireUp gunfire;
+    Projectile myprojectile;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        gunfire = GetComponent<FireUp>();
+        gunfire.YForce = 200f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M)) 
+        {
+            gunfire.Fire(rb);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Inheritance_Composition_Lecture/Assets/Scripts/Composition Scripts/FireUp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FireUp : MonoBehaviour
+{
+    public float YForce;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Fire(Rigidbody2D rb)
+    {
+        Vector2 myforce = new Vector2(0, YForce);
+        rb.AddForce(myforce);
+    }
+}
